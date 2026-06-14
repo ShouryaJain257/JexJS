9e6963db460ad94c22690c9987538d9e64329fbe
Shourya Jain <shouryaranu@gmail.com>
2026-06-14 20:52:07 +0530

    Update tc4.js

--- js-thunder-runtime/jsrunner/tests/tc4.js
+++ js-thunder-runtime/jsrunner/tests/tc4.js
@@ -1,4 +1,20 @@
-let arr = [1, 2, 3, 4, 5];
-let reversed = [...arr].reverse();
-console.log("Original: " + arr.join(", "));
-console.log("Reversed: " + reversed.join(", "));
+//Reversing the string
+function reverseString(str){
+    let arr = str.split("");
+    let n = arr.length;
+    let start = 0;
+    let end = n-1;
+
+    while(start<end){
+        let temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+
+    return arr.join("");
+}
+
+let revStr = reverseString('Hello');
+console.log(revStr);
