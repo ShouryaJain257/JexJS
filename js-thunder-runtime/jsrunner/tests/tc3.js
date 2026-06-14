d2a8b81abf6719880933671846edb0d054fbdae0
Shourya Jain <shouryaranu@gmail.com>
2026-06-14 14:51:06 +0530

    Update tc3.js

--- js-thunder-runtime/jsrunner/tests/tc3.js
+++ js-thunder-runtime/jsrunner/tests/tc3.js
@@ -1,12 +1,29 @@
+/*
+    Armstrong no. -> equals to sum of its digits raised to the power of number of digits 
+*/
 function isArmstrong(num) {
-    let temp = num;
+    let temp = Math.abs(num);
+    let absNum = Math.abs(num);// reference
     let sum = 0;
+
+    const digit_count = function(n){
+        let count = 0;
+        while(n!=0){
+            count += 1;
+            n = Math.floor(n/10);
+        }
+        return count;
+    }
+
+    let digits = digit_count(temp);
+    temp = absNum;
+    
     while (temp > 0) {
         let digit = temp % 10;
-        sum += digit ** 3;
+        sum += digit ** digits;
         temp = Math.floor(temp / 10);
     }
-    return sum === num;
+    return sum === absNum;
 }
 console.log(isArmstrong(153));
-console.log(isArmstrong(123));
+console.log(isArmstrong(-371));
